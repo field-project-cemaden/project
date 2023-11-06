2045ec5475d56a1c02734d0b2a9238717eab066c
Juan Belieni <juanbelieni@gmail.com>
2023-11-06 20:14:02 -0300

    Add buttons to change the layer

--- src/App.svelte
+++ src/App.svelte
@@ -1,75 +1,244 @@
 <script lang="ts">
-  import { onDestroy, onMount } from 'svelte';
-  import * as L from 'leaflet';
-  import { loadData } from './stores/data';
-  import IDWLayer from './components/idw-layer';
+import { onDestroy, onMount } from 'svelte';
+import * as L from 'leaflet';
+import { loadData } from './stores/data';
+import IDWLayer from './components/idw-layer';
+import {
+  AddOutline,
+  RemoveOutline,
+  LayersOutline,
+  MapOutline,
+} from 'svelte-ionicons';
 
-  const centerLatitude = -22.94;
-  const centerLongitude = -43.2;
+const centerLatitude = -22.94;
+const centerLongitude = -43.2;
 
-  let map: L.Map;
-  let mutationObserver: MutationObserver;
-  let onZoom: () => {};
-  let onMove: () => {};
-  let onHide: (hide: boolean) => {};
+enum Layer {
+  idw = 'Inverse distance weighting (IDW)',
+  gp = 'Gaussian process (GP)',
+}
 
-  onMount(() => {
-    map = L.map('map').setView([centerLatitude, centerLongitude], 10);
+let map: L.Map;
+let mutationObserver: MutationObserver;
+let onZoom = () => {};
+let onMove = () => {};
+let onHide = (_hide: boolean) => {};
 
-    L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
+let currentLayer = Layer.idw;
 
-    map.on('zoomstart', () => {
-      onHide(true);
-    });
+onMount(() => {
+  map = L.map('map').setView([centerLatitude, centerLongitude], 10);
 
-    map.on('zoomend', () => {
-      onZoom();
-      onHide(false);
-    });
+  L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 
-    mutationObserver = new MutationObserver(() => {
-      onMove();
-    });
-
-    mutationObserver.observe(document.querySelector('.leaflet-map-pane')!, {
-      attributes: true,
-      attributeFilter: ['style'],
-    });
-
-    loadData();
+  map.on('zoomstart', () => {
+    onHide(true);
   });
 
-  onDestroy(() => {
-    mutationObserver.disconnect();
+  map.on('zoomend', () => {
+    onZoom();
+    onHide(false);
   });
+
+  mutationObserver = new MutationObserver(() => {
+    onMove();
+  });
+
+  mutationObserver.observe(document.querySelector('.leaflet-map-pane')!, {
+    attributes: true,
+    attributeFilter: ['style'],
+  });
+
+  loadData();
+});
+
+onDestroy(() => {
+  mutationObserver.disconnect();
+});
+
+function onZoomIn() {
+  map.zoomIn();
+}
+
+function onZoomOut() {
+  map.zoomOut();
+}
 </script>
 
 <main>
   <div id="map">
-    <IDWLayer
-      {map}
-      bind:onZoom="{onZoom}"
-      bind:onMove="{onMove}"
-      bind:onHide="{onHide}"
-    />
+    {#if currentLayer == Layer.idw}
+      <IDWLayer {map} bind:onZoom bind:onMove bind:onHide />
+    {/if}
+
+    <aside>
+      <div class="button-group">
+        <button on:click={onZoomIn}>
+          <AddOutline />
+        </button>
+        <button on:click={onZoomOut}>
+          <RemoveOutline />
+        </button>
+      </div>
+
+      <div class="button-group">
+        <button id="layers-button">
+          <LayersOutline />
+
+          <ul id="layer-list">
+            {#each Object.values(Layer) as layer}
+              <li
+                class:selected={currentLayer == layer}
+                on:click={() => (currentLayer = layer)}
+                role="presentation"
+              >
+                <MapOutline />
+                <span>{layer}</span>
+              </li>
+            {/each}
+          </ul>
+        </button>
+      </div>
+    </aside>
   </div>
 </main>
 
 <style lang="scss">
-  main {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    #map {
-      width: 100%;
-      height: 100%;
-      user-select: none;
+  #map {
+    width: 100%;
+    height: 100%;
+    user-select: none;
+
+    :global(.leaflet-control-container) {
+      visibility: hidden;
     }
   }
+
+  aside {
+    position: absolute;
+    z-index: 10000;
+
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
+
+    top: 50%;
+    left: 0.5rem;
+    transform: translate(0, -50%);
+
+    .button-group {
+      display: flex;
+      flex-direction: column;
+      row-gap: 0.5rem;
+
+      button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        width: 3rem;
+        height: 3rem;
+        margin: 0;
+        padding: 0;
+        text-align: center;
+
+        background: white;
+        color: #283618;
+        border: none;
+        box-shadow: none;
+
+        :global(svg) {
+          outline: none;
+        }
+
+        &:hover {
+          background: #283618;
+          color: white;
+        }
+
+        &#layers-button {
+          position: relative;
+
+          &::after {
+            content: '';
+            width: 1rem;
+            height: 4rem;
+            left: 3rem;
+            position: absolute;
+          }
+
+          #layer-list {
+            visibility: hidden;
+            border-radius: var(--border-radius);
+
+            opacity: 0;
+            background: white;
+
+            position: absolute;
+            left: 4rem;
+            padding: 0.5rem 1rem;
+
+            transition: opacity var(--transition);
+
+            li {
+              display: flex;
+              align-items: center;
+              column-gap: 0.5rem;
+
+              width: 20rem;
+              margin: 0;
+              padding-left: 0;
+              border-bottom: 1px #28361844 solid;
+
+              font-weight: regular;
+              color: #000b;
+              transition: color var(--transition);
+
+              &:last-child {
+                border-bottom: 0;
+              }
+
+              :global(svg) {
+                padding: 5px;
+                width: 1.75rem;
+                height: 1.75rem;
+                border-radius: var(--border-radius);
+              }
+
+              &:hover:not(.selected) {
+                color: black;
+              }
+
+              &.selected {
+                color: black;
+
+                :global(svg) {
+                  color: white;
+                  background: #606c38;
+                }
+              }
+            }
+          }
+
+          &:hover {
+            #layer-list {
+              opacity: 1;
+              visibility: visible;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
--- src/components/idw-layer/index.svelte
+++ src/components/idw-layer/index.svelte
@@ -1,129 +1,132 @@
 <script lang="ts">
-  import { data } from '@/stores/data';
-  import * as d3 from 'd3';
-  import * as d3hex from 'd3-hexbin';
-  import * as L from 'leaflet';
-  import * as turf from '@turf/turf';
-  import { invDist, type Point } from './functions';
+import { data } from '@/stores/data';
+import * as d3 from 'd3';
+import * as d3hex from 'd3-hexbin';
+import * as L from 'leaflet';
+import * as turf from '@turf/turf';
+import { invDist, type Point } from './functions';
 
-  const deltaDegree = 4e-3;
+const deltaDegree = 4e-3;
 
-  export let map: L.Map;
+export let map: L.Map;
 
-  let svgEl: SVGSVGElement;
-  $: svg = d3.select(svgEl);
+let svgEl: SVGSVGElement;
+$: svg = d3.select(svgEl);
 
-  $: accumulatedData = $data.accumulated.map((row) => ({
-    position: [row.longitude, row.latitude] as [number, number],
-    value: row.acc120hr,
+$: accumulatedData = $data.accumulated.map((row) => ({
+  position: [row.longitude, row.latitude] as [number, number],
+  value: row.acc120hr,
+}));
+
+let points: Point[] = [];
+
+$: if ($data.isLoaded) {
+  const multyPolygon = turf.multiPolygon(
+    $data.boundary?.features[0].geometry.coordinates,
+  );
+  const [lon1, lat1, lon2, lat2] = turf.bbox(multyPolygon);
+
+  const positions: Array<[number, number]> = [];
+
+  for (let lat = lat1 + deltaDegree / 2; lat <= lat2; lat += deltaDegree) {
+    for (let lon = lon1 + deltaDegree / 2; lon <= lon2; lon += deltaDegree) {
+      positions.push([lon, lat]);
+    }
+  }
+
+  points = turf
+    .pointsWithinPolygon(turf.points(positions), multyPolygon)
+    .features.map((point) => point.geometry.coordinates as [number, number])
+    .map((point) => ({
+      position: point,
+      value: invDist(point, accumulatedData),
+    }));
+}
+
+function transform([lon, lat]: [number, number]): [number, number] {
+  const point = map.latLngToLayerPoint(new L.LatLng(lat, lon));
+  return [point.x, point.y];
+}
+
+function drawHeatMap() {
+  if (points.length < 2) return;
+
+  const transformedPoints = points.map((point) => ({
+    ...point,
+    position: transform(point.position),
   }));
 
-  let points: Point[] = [];
+  const color = d3
+    .scaleQuantize<string>()
+    .domain([0, d3.max([...accumulatedData.map((point) => point.value), 1])!])
+    .range(d3.schemeBlues[9]);
 
-  $: if ($data.isLoaded) {
-    const multyPolygon = turf.multiPolygon(
-      $data.boundary?.features[0].geometry.coordinates,
+  const radius =
+    80 *
+    deltaDegree *
+    Math.abs(
+      transformedPoints[1].position[0] - transformedPoints[0].position[0],
     );
-    const [lon1, lat1, lon2, lat2] = turf.bbox(multyPolygon);
 
-    const positions: Array<[number, number]> = [];
+  const hexbin = d3hex
+    .hexbin<Point>()
+    .x((d) => d.position[0])
+    .y((d) => d.position[1])
+    .radius(radius);
 
-    for (let lat = lat1 + deltaDegree / 2; lat <= lat2; lat += deltaDegree) {
-      for (let lon = lon1 + deltaDegree / 2; lon <= lon2; lon += deltaDegree) {
-        positions.push([lon, lat]);
-      }
-    }
+  console.log(svg.select('g'));
 
-    points = turf
-      .pointsWithinPolygon(turf.points(positions), multyPolygon)
-      .features.map((point) => point.geometry.coordinates as [number, number])
-      .map((point) => ({
-        position: point,
-        value: invDist(point, accumulatedData),
-      }));
-  }
+  svg
+    .select('g')
+    .selectAll('path')
+    .data(hexbin(transformedPoints))
+    .join('path')
+    .attr('transform', (d) => `translate(${d.x},${d.y})`)
+    .attr('d', hexbin.hexagon())
+    .attr('fill', (d) => {
+      const mean = d.reduce((acc, point) => acc + point.value, 0) / d.length;
+      return color(mean);
+    })
+    .attr('opacity', 0.7);
+}
 
-  function transform([lon, lat]: [number, number]): [number, number] {
-    const point = map.latLngToLayerPoint(new L.LatLng(lat, lon));
-    return [point.x, point.y];
-  }
+function translateG() {
+  const container = map.getContainer().querySelector('.leaflet-map-pane')!;
+  const rect = container.getBoundingClientRect();
+  const translate = [rect.x, rect.y];
 
-  function drawHeatMap() {
-    if (points.length < 2) return;
+  svg
+    .select('g')
+    .attr('transform', `translate(${translate[0]}, ${translate[1]})`);
+}
 
-    const transformedPoints = points.map((point) => ({
-      ...point,
-      position: transform(point.position),
-    }));
+export function onZoom() {
+  drawHeatMap();
+}
 
-    const color = d3
-      .scaleQuantize<string>()
-      .domain([0, d3.max([...accumulatedData.map((point) => point.value), 1])!])
-      .range(d3.schemeBlues[9]);
+export function onMove() {
+  translateG();
+}
 
-    const radius =
-      80 *
-      deltaDegree *
-      Math.abs(
-        transformedPoints[1].position[0] - transformedPoints[0].position[0],
-      );
+export function onHide(hide: boolean) {
+  svg.style('visibility', hide ? 'hidden' : 'visible');
+}
 
-    const hexbin = d3hex
-      .hexbin<Point>()
-      .x((d) => d.position[0])
-      .y((d) => d.position[1])
-      .radius(radius);
-
-    svg
-      .select('g')
-      .selectAll('path')
-      .data(hexbin(transformedPoints))
-      .join('path')
-      .attr('transform', (d) => `translate(${d.x},${d.y})`)
-      .attr('d', hexbin.hexagon())
-      .attr('fill', (d) => {
-        const mean = d.reduce((acc, point) => acc + point.value, 0) / d.length;
-        return color(mean);
-      })
-      .attr('opacity', 0.5);
-  }
-
-  function translateG() {
-    const container = map.getContainer().querySelector('.leaflet-map-pane')!;
-    const rect = container.getBoundingClientRect();
-    const translate = [rect.x, rect.y];
-
-    svg
-      .select('g')
-      .attr('transform', `translate(${translate[0]}, ${translate[1]})`);
-  }
-
-  export function onZoom() {
-    drawHeatMap();
-  }
-
-  export function onMove() {
-    translateG();
-  }
-
-  export function onHide(hide: boolean) {
-    svg.style('visibility', hide ? 'hidden' : 'visible');
-  }
-
-  $: if ($data.isLoaded) {
-    drawHeatMap();
-  }
+$: if ($data.isLoaded && svg) {
+  drawHeatMap();
+  translateG();
+}
 </script>
 
-<svg bind:this="{svgEl}">
+<svg bind:this={svgEl}>
   <g></g>
 </svg>
 
 <style lang="scss">
-  svg {
-    width: 100vw;
-    height: 100vh;
-    position: absolute;
-    z-index: 1000;
-  }
+svg {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  z-index: 1000;
+}
 </style>
